1903467de17f57972af154b69ee027c566b68d90
Christina <chu.christina@yahoo.com>
2018-11-09 15:46:20 -0800

    Write setter and getter methods for SQLObject class

---
+++ active-record-lite-1/lib/01_sql_object.rb
@@ -0,0 +1,105 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+require 'byebug'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+
+  def self.columns
+    return @columns if @columns
+    query = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+
+    @columns = query.first.map{|col| col.to_sym}
+  end
+
+  def self.finalize!
+    self.columns.each do |col|
+      define_method("#{col}") do
+        attributes["#{col}".to_sym]
+      end
+
+      define_method("#{col}=") do |value|
+        attributes["#{col}".to_sym] = value
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= self.to_s.tableize
+  end
+
+  def self.all
+    result = DBConnection.execute(<<-SQL)
+      SELECT * FROM "#{table_name}"
+    SQL
+    self.parse_all(result)
+  end
+
+  def self.parse_all(results)
+    results.map do |hash|
+      self.new(hash)
+    end
+  end
+
+  def self.find(id)
+    result = DBConnection.execute(<<-SQL, id)
+      SELECT *
+      FROM #{table_name}
+      WHERE #{table_name}.id = ?
+    SQL
+
+    return nil if result.empty?
+    self.new(result.first)
+  end
+
+  def initialize(params = {})
+    params.each do |attr_name, value|
+      raise "unknown attribute '#{attr_name}'" unless self.class.columns.include?(attr_name.to_sym)
+      self.send("#{attr_name}=", value)
+    end
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    self.class.columns.map do |col| # col is sym
+      self.send(col)
+    end
+  end
+
+  def insert
+    col_names = self.class.columns.join(",")
+    question_marks = (["?"] * self.class.columns.length).join(",")
+    question_marks_with_paren = "(#{question_marks})"
+    attr_values = attribute_values.join(",")
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        #{question_marks_with_paren}
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
