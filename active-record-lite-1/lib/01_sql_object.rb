bc75a072e2649c9bb89a0a7e899b6124c8400b71
Christina <chu.christina@yahoo.com>
2018-11-09 16:24:41 -0800

    Finish #insert, #update, and #save for SQLObject class

--- active-record-lite-1/lib/01_sql_object.rb
+++ active-record-lite-1/lib/01_sql_object.rb
@@ -82,24 +82,30 @@ class SQLObject
   def insert
     col_names = self.class.columns.join(",")
     question_marks = (["?"] * self.class.columns.length).join(",")
-    question_marks_with_paren = "(#{question_marks})"
-    attr_values = attribute_values.join(",")
 
     DBConnection.execute(<<-SQL, *attribute_values)
       INSERT INTO
         #{self.class.table_name} (#{col_names})
       VALUES
-        #{question_marks_with_paren}
+        (#{question_marks})
     SQL
 
     self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    set_line = self.class.columns.map {|col| "#{col} = ?"}.join(",")
+    DBConnection.execute(<<-SQL, *attribute_values)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        id = #{self.id}
+    SQL
   end
 
   def save
-    # ...
+    id ? update : insert
   end
 end
--- active-record-lite-1/lib/02_searchable.rb
+++ active-record-lite-1/lib/02_searchable.rb
@@ -3,10 +3,19 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map {|key| "#{key} = ?"}.join(" AND ")
+    params_values = params.values
+    DBConnection.execute(<<-SQL, *params_values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
